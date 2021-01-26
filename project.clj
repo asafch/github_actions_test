428f0cc44db1ce1ea778d195f50769cdfc5bddec
asaf.chelouche <asaf.chelouche@appsflyer.com>
2021-01-26 12:20:02 +0200

    Auto-deploy snapshots

--- project.clj
+++ project.clj
@@ -3,6 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
+  :deploy-repositories [["releases" {:url           "https://repo.clojars.org"
+                                     :username      :env/clojars_username
+                                     :password      :env/clojars_password
+                                     :sign-releases false}]]
   :dependencies [[org.clojure/clojure "1.10.0"]]
   :profiles {:dev     {:plugins      [[lein-eftest "0.5.9"]]
                        :dependencies [[org.clojure/clojure "1.10.1"]
