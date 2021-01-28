e72180cee2e108dd3acde2fbeb178ce9197d61b5
asaf.chelouche <asaf.chelouche@appsflyer.com>
2021-01-28 10:29:05 +0200

    0.3.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.asafch/github_actions_test "0.3.0"
+(defproject org.clojars.asafch/github_actions_test "0.3.1-SNAPSHOT"
   :description "FIXME: write description"
   :url "https://github.com/asafch/github_actions_test"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
@@ -13,9 +13,10 @@
                                       :sign-releases false}]]
   :dependencies [[org.clojure/clojure "1.10.0"]]
   :profiles {:dev     {:plugins      [[lein-eftest "0.5.9"]]
-                       :eftest       {:multithread?   false
-                                      :report         eftest.report.junit/report
-                                      :report-to-file "target/junit.xml"}
+                       :eftest       {:multithread?    false
+                                      :capture-output? false
+                                      :report          eftest.report.junit/report
+                                      :report-to-file  "target/junit.xml"}
                        :dependencies [[org.clojure/clojure "1.10.1"]
                                       [clj-kondo "RELEASE"]]
                        :aliases      {"clj-kondo" ["run" "-m" "clj-kondo.main"]
